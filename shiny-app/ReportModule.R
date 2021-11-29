b0a70897b7bec402ba0174cba5ccb8d05a03ef40
Oddmund Huseby <huseby.oddmund@gmail.com>
2021-11-29 12:58:51 +0100

    added loading icon when downloading report

--- shiny-app/ReportModule.R
+++ shiny-app/ReportModule.R
@@ -1,8 +1,8 @@
 reportUI <- function(id) {
   ns <- NS(id)
   tagList(
-    verbatimTextOutput(ns('argList')),
-    downloadButton(ns("report"), "Generate report"),
+    verbatimTextOutput(ns("argList")),
+    downloadButton(ns("report"), "Generate report")
   )
 }
 
@@ -12,33 +12,46 @@ reportServer <- function(id, insertions, removals, myValues) {
     id,
     function(input, output, session) {
       ns <- session$ns
-      
-      output$argList<-renderPrint({
+
+      output$argList <- renderPrint({
         print(myValues$dList)
       })
-      
+
       output$report <- downloadHandler(
         # For PDF output, change this to "report.pdf"
         filename = "report.html",
         content = function(file) {
-          # Copy the report file to a temporary directory before processing it, in
-          # case we don't have write permissions to the current working dir (which
-          # can happen when deployed).
-          tempReport <- file.path(tempdir(), "report.Rmd")
-          file.copy("shiny-app/report.Rmd", tempReport, overwrite = TRUE)
-          
-          # Set up parameters to pass to Rmd document
-          params <- list(plotArgs = myValues$dList,
-                         insertions = insertions, removals = removals)
-          
-          # Knit the document, passing in the `params` list, and eval it in a
-          # child of the global environment (this isolates the code in the document
-          # from the code in this app).
-          rmarkdown::render(tempReport, output_file = file,
-                            params = params,
-                            envir = new.env(parent = globalenv())
+          withProgress(
+            message = paste0("Downloading custom report"),
+            value = 0,
+            {
+              shiny::incProgress(1 / 10)
+              # Copy the report file to a temporary directory before processing it, in
+              # case we don't have write permissions to the current working dir (which
+              # can happen when deployed).
+              tempReport <- file.path(tempdir(), "report.Rmd")
+              file.copy("shiny-app/report.Rmd", tempReport, overwrite = TRUE)
+
+              shiny::incProgress(2 / 10)
+              # Set up parameters to pass to Rmd document
+              params <- list(
+                plotArgs = myValues$dList,
+                insertions = insertions, removals = removals
+              )
+
+              shiny::incProgress(5 / 10)
+              # Knit the document, passing in the `params` list, and eval it in a
+              # child of the global environment (this isolates the code in the document
+              # from the code in this app).
+              rmarkdown::render(tempReport,
+                output_file = file,
+                params = params,
+                envir = new.env(parent = globalenv())
+              )
+            }
           )
         }
       )
-    })
+    }
+  )
 }
--- shiny-app/StdReportModule.R
+++ shiny-app/StdReportModule.R
@@ -8,47 +8,57 @@ stdReportUI <- function(id) {
 
 
 stdReportServer <- function(id, insertions,
-                         removals,
-                         myValues) {
+                            removals,
+                            myValues) {
   moduleServer(
     id,
     function(input, output, session) {
       ns <- session$ns
-      
+
       clinics <- removals %>%
         distinct(Clinic) %>%
         arrange(Clinic) %>%
         mutate(Clinic = as.character(Clinic))
-      
+
       output$selectClinic <- renderUI({
         pickerInput(ns("selectClinic"),
-                    "Select Clinic",
-                    choices = clinics
+          "Select Clinic",
+          choices = clinics
         )
       })
-      
+
       output$stdReport <- downloadHandler(
         # For PDF output, change this to "report.pdf"
         filename = "stdReport.html",
         content = function(file) {
-          # Copy the report file to a temporary directory before processing it, in
-          # case we don't have write permissions to the current working dir (which
-          # can happen when deployed).
-          tempReport <- file.path(tempdir(), "stdReport.Rmd")
-          file.copy("shiny-app/stdReport.Rmd", tempReport, overwrite = TRUE)
-          
-          # Set up parameters to pass to Rmd document
-          params <- list(clinic = input$selectClinic, insertions = insertions, removals = removals)
-          
-          # Knit the document, passing in the `params` list, and eval it in a
-          # child of the global environment (this isolates the code in the document
-          # from the code in this app).
-          rmarkdown::render(tempReport, output_file = file,
-                            params = params,
-                            envir = new.env(parent = globalenv())
+          withProgress(
+            message = paste0("Downloading report"),
+            value = 0,
+            {
+              shiny::incProgress(1 / 10)
+              # Copy the report file to a temporary directory before processing it, in
+              # case we don't have write permissions to the current working dir (which
+              # can happen when deployed).
+              tempReport <- file.path(tempdir(), "stdReport.Rmd")
+              file.copy("shiny-app/stdReport.Rmd", tempReport, overwrite = TRUE)
+
+              shiny::incProgress(2 / 10)
+              # Set up parameters to pass to Rmd document
+              params <- list(clinic = input$selectClinic, insertions = insertions, removals = removals)
+
+              shiny::incProgress(5 / 10)
+              # Knit the document, passing in the `params` list, and eval it in a
+              # child of the global environment (this isolates the code in the document
+              # from the code in this app).
+              rmarkdown::render(tempReport,
+                output_file = file,
+                params = params,
+                envir = new.env(parent = globalenv())
+              )
+            }
           )
         }
       )
-  
-    })
+    }
+  )
 }
